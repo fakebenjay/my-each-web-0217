c44ec543d1d792384b4d5ab41e680534e3e2ba7c
Ben Jay <fakebenjay@gmail.com>
2017-01-24 06:18:48 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
-  # code here
+collection = [1, 2, 3, 4]
+
+def my_each(collection)
+  recollection = []
+  i = 0
+  while i < collection.length
+    yield collection[i]
+      i += 1
+    recollection << collection[i-1]
+  end
+  return recollection
+end
+
+my_each(collection) do |i|
 end
