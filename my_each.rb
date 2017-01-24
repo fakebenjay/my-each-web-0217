cb9e298c9930473d274332ede0d7df140cbf0c84
Ben Jay <fakebenjay@gmail.com>
2017-01-24 07:26:36 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -2,18 +2,17 @@ collection = [1, 2, 3, 4]
 
 def my_each(collection)
   if block_given?
-    recollection = []
     i = 0
     while i < collection.length
       yield collection[i]
         i += 1
-        recollection << collection[i-1]
     end
-    return recollection
+    return collection
   else
     return "Oops!"
   end
 end
 
 my_each(collection) do |i|
+  #PUTS i
 end
