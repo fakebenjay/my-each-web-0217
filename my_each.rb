a8edc91d6f79dd9235b32eb1f84e8ac04acf6f7c
Ben Jay <fakebenjay@gmail.com>
2017-01-24 06:21:14 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,14 +1,18 @@
 collection = [1, 2, 3, 4]
 
 def my_each(collection)
-  recollection = []
-  i = 0
-  while i < collection.length
-    yield collection[i]
-      i += 1
-    recollection << collection[i-1]
+  if block_given?
+    recollection = []
+    i = 0
+    while i < collection.length
+      yield collection[i]
+        i += 1
+        recollection << collection[i-1]
+    end
+    return recollection
+  else
+    return "Oops!"
   end
-  return recollection
 end
 
 my_each(collection) do |i|
